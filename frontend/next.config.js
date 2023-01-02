36654c4f78b88cdbbfec0adb9d5d103182bf05ca
Simon Schippl <honeybadger26@gmail.com>
2023-01-02 23:43:16 +1100

    Enable hot reloading for frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-    env: {
-        api: {
-            server: "http://api:3001",
-            client: "http://localhost:3001",
-        }
+  env: {
+    api: {
+      server: "http://api:3001",
+      client: "http://localhost:3001",
+    }
+  },
+  webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 300,
     },
+  }),
 }
