160f90564ee4fdd0acc1bbb4fb002395fd432102
Simon Schippl <honeybadger26@gmail.com>
2023-01-02 23:42:52 +1100

    Add tailwindcss

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,28 +3,32 @@ import { useState } from "react";
 const API = process.env.api;
 
 export async function getServerSideProps() {
-    const res = await fetch(`${API.server}/get`);
+  const res = await fetch(`${API.server}/get`);
+  const data = await res.json();
+
+  return { props: { initialDate: data.date } };
+}
+
+export default function({ initialDate }) {
+  const [date, setDate] = useState(initialDate);
+  
+  async function handleClick() {
+    const res = await fetch(`${API.client}/set`, { method: "POST" });
     const data = await res.json();
 
-    return { props: { initialDate: data.date } };
+    setDate(data.date);
+  }
+  
+  return (
+    <div className="p-5">
+      { date && <p className="pb-3 font-mono">Last clicked: {date}</p> }
+      <button
+        className="text-3xl p-3 font-sans rounded-lg text-slate-50 bg-blue-600 hover:bg-blue-500 active:bg-blue-400"
+        onClick={handleClick}
+      >
+        You won't click me. You're too scared
+      </button>
+    </div>
+  );
 }
-
-export default function Home({ initialDate }) {
-    const [date, setDate] = useState(initialDate);
-
-    async function updateDate() {
-        const res = await fetch(`${API.client}/set`, { method: "POST" });
-        const data = await res.json();
-
-        setDate(data.date);
-    }
-
-    return (
-        <div>
-            { date && <p>Last clicked: {date}</p> }
-            <button onClick={() => updateDate()}>
-                You won't click me. You're too scared.
-            </button>
-        </div>
-    );
-}
+  
