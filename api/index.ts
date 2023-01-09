ee5d42b528314815d4337ab1a977906f384e1755
Simon Schippl <honeybadger26@gmail.com>
2023-01-09 12:24:07 +1100

    Add Redis
    Last Clicked Date is now saved in Redis. This commit also adds improvements to the Docker setup.

--- api/index.ts
+++ api/index.ts
@@ -1,32 +1,38 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
+import { createClient } from 'redis';
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+
+const LAST_CLICKED = 'lastClicked';
 
 const app = express();
+const redis = createClient({ url: 'redis://db:6379' });
 
-app.use(cors({ origin: "*", methods: ["GET", "POST"] }));
+app.use(cors({ origin: '*', methods: ['GET', 'POST'] }));
 
 app.use((req: Request, _res: Response, next: () => void) => {
     console.log(req.method, req.originalUrl);
     next();
 });
 
-var date: Date;
+app.get('/get', async (_req: Request, res: Response) => {
+    const date = await redis.get(LAST_CLICKED);
 
-app.get("/get", (_req: Request, res: Response) => {
-    if (date) {
-        res.json({ date });
-    } else {
-        res.json({ date: null });
-    }
+    if (date) res.json({ date });
+    else res.json({ date: null });
 });
 
-app.post("/set", (req: Request, res: Response) => {
-    date = new Date();
+app.post('/set', async (_req: Request, res: Response) => {
+    const date = new Date();
+    await redis.set(LAST_CLICKED, date.toISOString());
     res.json({ date });
 });
 
 const port = 3001;
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Server running at http://localhost:${port}`);
+
+    // Setup redis
+    redis.on('error', (error: any) => console.log('Redis Client Error', error));
+    await redis.connect();
 });
